1e49f0208399ee28649ce32b3ec2fd633f911c7d
EllaiSys <admin@ellaisys.com>
2026-03-28 14:22:36 +0530

    refactor: remove unused exceptions and improve error handling in token helper

--- src/modules/base/helpers/token.py
+++ src/modules/base/helpers/token.py
@@ -1,17 +1,11 @@
-import jwt
+""" Import the required modules """
 from datetime import datetime, timedelta, timezone
+import jwt
+
+# Import the required modules from the project
 from modules.base.config import config
 from modules.base.models.auth import AccessToken
-
-class DecodeTokenException(Exception):
-    code = 400
-    error_code = "TOKEN__DECODE_ERROR"
-    message = "token decode error"
-
-class ExpiredTokenException(Exception):
-    code = 400
-    error_code = "TOKEN__EXPIRE_TOKEN"
-    message = "expired token"
+from modules.base.exceptions import InvalidTokenException
 
 class TokenHelper:
     @staticmethod
@@ -19,26 +13,29 @@ class TokenHelper:
             payload: dict, subject: str|None=None,
             expire_period: int = 3600
         ) -> AccessToken:
-        # Set the expiration time
-        expires_at: datetime = datetime.now(tz=timezone.utc) + timedelta(seconds=expire_period)
+        try:
+            # Set the expiration time
+            expires_at: datetime = datetime.now(tz=timezone.utc) + timedelta(seconds=expire_period)
 
-        # Set the payload
-        token: AccessToken = AccessToken()
-        token.access_token = jwt.encode(
-            payload={
-                **payload,
-                "exp": expires_at,                      # expiration time
-                "iat": datetime.now(tz=timezone.utc),   # issued at
-                "nbf": datetime.now(tz=timezone.utc),   # not before
-                "iss": config.JWT_ISSUER,               # issuer
-                "aud": config.JWT_AUDIENCE,             # audience
-                "sub": subject,                         # subject
-            },
-            key=config.JWT_SECRET_KEY,
-            algorithm=config.JWT_ALGORITHM,
-        )
-        token.expires_at = int(expires_at.timestamp())
-        return token
+            # Set the payload
+            token: AccessToken = AccessToken()
+            token.access_token = jwt.encode(
+                payload={
+                    **payload,
+                    "exp": expires_at,                      # expiration time
+                    "iat": datetime.now(tz=timezone.utc),   # issued at
+                    "nbf": datetime.now(tz=timezone.utc),   # not before
+                    "iss": config.JWT_ISSUER,               # issuer
+                    "aud": config.JWT_AUDIENCE,             # audience
+                    "sub": subject,                         # subject
+                },
+                key=config.JWT_SECRET_KEY,
+                algorithm=config.JWT_ALGORITHM,
+            )
+            token.expires_at = int(expires_at.timestamp())
+            return token
+        except (jwt.exceptions.PyJWTError, Exception) as exc:
+            raise InvalidTokenException(str(exc)) from exc
 
     @staticmethod
     def decode(token: str) -> dict:
@@ -50,10 +47,12 @@ class TokenHelper:
                 audience=config.JWT_AUDIENCE,
                 issuer=config.JWT_ISSUER
             )
-        except jwt.exceptions.DecodeError as exc:
-            raise DecodeTokenException from exc
         except jwt.exceptions.ExpiredSignatureError as exc:
-            raise ExpiredTokenException from exc
+            raise InvalidTokenException(
+                    'Token expired', 'error_code_expired_token'
+                ) from exc
+        except (jwt.exceptions.PyJWTError, Exception) as exc:
+            raise InvalidTokenException(str(exc)) from exc
 
     @staticmethod
     def decode_expired_token(token: str) -> dict:
@@ -64,5 +63,5 @@ class TokenHelper:
                 config.JWT_ALGORITHM,
                 options={"verify_exp": False},
             )
-        except jwt.exceptions.DecodeError as exc:
-            raise DecodeTokenException from exc
+        except (jwt.exceptions.PyJWTError, Exception) as exc:
+            raise InvalidTokenException(str(exc)) from exc
